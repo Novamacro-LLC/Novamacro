5a5ca4dcf0ff21493c992208545db7a9550ad81d
fbdavison <80009345+fbdavison@users.noreply.github.com>
2024-11-24 13:00:08 -0600

    updated home page

--- public/views.py
+++ public/views.py
@@ -1,3 +1,15 @@
+from lib2to3.fixes.fix_input import context
+
 from django.shortcuts import render
 
-# Create your views here.
+
+#Create view for the home page
+def home(request):
+    title = 'Novamacro, LLC Home'
+    desc = ('At Novamacro, we help small and medium businesses harness the power of emerging technologies to drive '
+            'growth, increase efficiency, and unlock new opportunities.')
+    context = {
+        'title': title,
+        'desc': desc,
+    }
+    return render(request, 'public/index.html', context)
