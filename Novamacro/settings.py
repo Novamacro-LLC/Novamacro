89ae450bb3f5caea543a1fe11e33551846201b24
fbdavison <80009345+fbdavison@users.noreply.github.com>
2024-11-24 18:05:40 -0600

    updated settings for deployment

--- Novamacro/settings.py
+++ Novamacro/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m_67*@qynx6v2=p+#vh4ewwy(gq&cp50s-jm6@31y*0im35b)a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
 # Application definition
